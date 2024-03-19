2a16f26882d0bd3a264d89c96d7330c96ac0be0f
JAIME P.C.I <jaimeolarte888@gmail.com>
2024-03-18 23:37:04 -0500

    error arreglado en logica de modelo

--- src/Modelo/Modelo.java
+++ src/Modelo/Modelo.java
@@ -1,18 +1,25 @@
 package Modelo;
 
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Modelo {
     private List<Linea> lineas;
     private List<Linea> lineasBorradas;
+    private boolean undoRealizado;
 
     public Modelo() {
         lineas = new ArrayList<>();
         lineasBorradas = new ArrayList<>();
+        undoRealizado = false;
     }
 
     public void agregarLinea(Linea linea) {
+        if (undoRealizado) {
+            lineasBorradas.clear();
+            undoRealizado = false;
+        }
         lineas.add(linea);
     }
 
@@ -24,13 +31,15 @@ public class Modelo {
         if (!lineas.isEmpty()) {
             Linea ultimaLinea = lineas.remove(lineas.size() - 1);
             lineasBorradas.add(ultimaLinea);
+            undoRealizado = true;
         }
     }
 
     public void redo() {
-        if (!lineasBorradas.isEmpty()) {
+        if (undoRealizado && !lineasBorradas.isEmpty()) {
             Linea ultimaLineaBorrada = lineasBorradas.remove(lineasBorradas.size() - 1);
             lineas.add(ultimaLineaBorrada);
         }
     }
 }
+
